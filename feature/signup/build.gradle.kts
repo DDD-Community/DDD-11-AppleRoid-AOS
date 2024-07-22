692d8ba25a609ec8380bd1abdcf92c31b4c71dbb
hoon-bskr <isanghoony@gmail.com>
2024-07-22 20:56:36 +0900

    [Feat] (Signup): add signup feature module
    feature:signup 모듈을 추가하고
    네비게이션과 스크린 기본 설정을 완료했습니다.

---
+++ feature/signup/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.mkung.android.feature.ui)
+    alias(libs.plugins.mkung.android.library.compose)
+}
+
+android {
+    namespace = "com.appleroid.feature.signup"
+}
+
+dependencies {
+    implementation(projects.core.data)
+}
