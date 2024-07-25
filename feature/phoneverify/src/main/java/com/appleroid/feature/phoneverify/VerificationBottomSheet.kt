be87d572c1f61c90d90adb48626446bed4e8f39c
hoon-bskr <isanghoony@gmail.com>
2024-07-26 02:31:16 +0900

    [Style] (Phoneverify) : bottom sheet UI complete
    [Style] (Phoneverify) : 타이머를 제외한
    바텀시트 기본 UI 구성을 완료했습니다.

--- feature/phoneverify/src/main/java/com/appleroid/feature/phoneverify/VerificationBottomSheet.kt
+++ feature/phoneverify/src/main/java/com/appleroid/feature/phoneverify/VerificationBottomSheet.kt
@@ -1,31 +1,47 @@
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,9 +53,11 @@ fun VerificationBottomSheet(
     sheetState: Boolean,
     setSheetState: (Boolean) -> Unit
 ) {
-    LaunchedEffect(sheetState) {
+    var text by remember { mutableStateOf("") }
+
+    /*LaunchedEffect(sheetState) {
         if (!sheetState) setSheetState(false)
-    }
+    }*/
 
     if (sheetState) {
         ModalBottomSheet(
@@ -51,12 +69,17 @@ fun VerificationBottomSheet(
         ) {
             Box {
                 Image(
-                    modifier = Modifier.align(Alignment.TopEnd).padding(top = 16.dp, end = 16.dp),
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(top = 16.dp, end = 16.dp)
+                        .clickable {
+                            setSheetState(false)
+                        },
                     painter = painterResource(R.drawable.ic_round_close),
                     contentDescription = "인증번호 입력 바텀 시트 닫기 이미지"
                 )
                 Column(
-                    modifier = Modifier.padding(start = 25.dp, top = 25.dp, end = 25.dp)
+                    modifier = Modifier.padding(start = 25.dp, top = 25.dp, end = 25.dp, bottom = 60.dp)
                         .fillMaxWidth()
                 ) {
                     Text(
@@ -69,7 +92,7 @@ fun VerificationBottomSheet(
                     )
 
                     Text(
-                        modifier = Modifier.padding(top = 25.dp),
+                        modifier = Modifier.padding(top = 25.dp, bottom = 5.dp),
                         text = stringResource(R.string.bottom_sheet_verify_code),
                         style = TextStyle(
                             fontSize = 12.sp,
@@ -79,15 +102,77 @@ fun VerificationBottomSheet(
                         )
                     )
 
-                    Row(modifier = Modifier.padding(top = 5.dp, bottom = 60.dp)) {
-                        Box(
-                            modifier = Modifier
-                                .height(42.dp)
-                                .fillMaxWidth()
-                                .clip(RoundedCornerShape(12.dp))
-                                .background(colorResource(R.color.card_background)),
-                        ) {
+                    Box(
+                        modifier = Modifier
+                            .height(42.dp)
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(colorResource(R.color.card_background))
+                    ) {
+                        Row (modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 14.dp, end = 14.dp)) {
 
+                            BasicTextField(
+                                modifier = Modifier
+                                    .wrapContentWidth()
+                                    .align(Alignment.CenterVertically)
+                                    .background(Color.Transparent),
+                                value = text,
+                                cursorBrush = SolidColor(Color.Transparent),
+                                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.NumberPassword),
+                                onValueChange = {
+                                    if (it.length <= 6) {
+                                        text = it
+                                    }
+                                },
+                                textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+                                singleLine = true,
+                            )
+
+                            Spacer(modifier = Modifier.weight(1f))
+
+                            Text(
+                                modifier = Modifier
+                                    .align(Alignment.CenterVertically)
+                                    .padding(end = 6.dp),
+                                text = "02:59",
+                                style = TextStyle(
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = colorResource(R.color.button_border),
+                                )
+                            )
+
+                            Card(
+                                modifier = Modifier
+                                    .width(50.dp)
+                                    .height(23.dp)
+                                    .border(
+                                        1.dp,
+                                        colorResource(R.color.color_border_grey),
+                                        RoundedCornerShape(17.dp)
+                                    )
+                                    .align(Alignment.CenterVertically),
+
+                                colors = CardDefaults.cardColors(
+                                    containerColor = colorResource(R.color.card_background)
+                                )
+                            ) {
+                                Box(
+                                    contentAlignment = Alignment.Center,
+                                    modifier = Modifier.fillMaxSize()
+                                ) {
+                                    Text(
+                                        text = stringResource(R.string.re_send),
+                                        style = TextStyle(
+                                            fontSize = 12.sp,
+                                            fontWeight = FontWeight.Normal,
+                                            color = Color.White,
+                                        )
+                                    )
+                                }
+                            }
                         }
                     }
                 }
