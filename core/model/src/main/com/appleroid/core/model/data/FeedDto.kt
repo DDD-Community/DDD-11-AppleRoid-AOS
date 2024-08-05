73ccbec659b6dce1998a831915b25e20719405e0
isanghoony <isanghoony@gmail.com>
2024-08-06 02:04:17 +0900

    [Feat] (data) FeedInfoDto 추가

---
+++ core/model/src/main/com/appleroid/core/model/data/FeedDto.kt
@@ -0,0 +1,10 @@
+package com.appleroid.core.model.data
+
+data class FeedInfoDto (
+    val nickName: String,
+    val profileImageUrl: String,
+    val mbti: String,
+    val time: String,
+    val feedTitle: String,
+    val feedContent: String
+)
