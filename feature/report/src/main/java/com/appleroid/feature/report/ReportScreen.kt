8cc0e62ed60920647717f0c5263a64103e9662d3
isanghoony <isanghoony@gmail.com>
2024-08-22 22:54:37 +0900

    [Refactor] (feature:report) report screen refactoring

--- feature/report/src/main/java/com/appleroid/feature/report/ReportScreen.kt
+++ feature/report/src/main/java/com/appleroid/feature/report/ReportScreen.kt
@@ -1,31 +1,28 @@
 package com.appleroid.feature.report
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -36,24 +33,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.appleroid.core.designsystem.component.MKungBtn
 import com.appleroid.core.designsystem.component.MKungTextField
 import com.appleroid.core.designsystem.component.TitleText
 import com.appleroid.core.designsystem.component.WithTextCheckBox
-import com.appleroid.core.designsystem.component.WithTextCheckBoxCard
 import com.appleroid.core.designsystem.theme.BLACK
 import com.appleroid.core.designsystem.theme.BTN_BACKGROUND
-import com.appleroid.core.designsystem.theme.GREY01
+import com.appleroid.core.designsystem.theme.DOT
 import com.appleroid.core.designsystem.theme.GREY03
-import com.appleroid.core.designsystem.utils.PhoneNumberVisualTransformation
-import com.appleroid.core.ui.FeedCard
+import com.appleroid.core.designsystem.theme.GREY05
+import com.appleroid.core.designsystem.theme.POINT01
 import com.appleroid.model.Report
-import kotlinx.coroutines.launch
 
 @Composable
 fun ReportRoute(
@@ -76,7 +71,10 @@ fun ReportScreen(
 ) {
     var selectedId by remember { mutableIntStateOf(0) }
     var reportComment by remember { mutableStateOf("") }
-    val lastItemId = reportList.lastOrNull { it.title == "기타" }?.id ?: - 1
+    val lastItemId = reportList.lastOrNull { it.title == "기타" }?.id ?: -1
+
+    val isKeyboardVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+    val enableSubmit = selectedId != 0 && (selectedId != lastItemId || reportComment.isNotBlank())
 
     Box(
         modifier = modifier
@@ -84,24 +82,22 @@ fun ReportScreen(
             .windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier = Modifier.fillMaxWidth()
         ) {
             TopBar(backBtnClicked = backBtnClicked)
 
-            TitleText(
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(start = 20.dp, top = 16.dp),
-                title = stringResource(R.string.report_reason_select)
-            )
-
             LazyColumn(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(start = 20.dp, end = 20.dp, top = 18.dp)
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp)
             ) {
+                item {
+                    TitleText(
+                        modifier = Modifier
+                            .padding(top = 16.dp, bottom = 18.dp),
+                        title = stringResource(R.string.report_reason_select)
+                    )
+                }
                 items(reportList, key = { it.id }) { item ->
                     WithTextCheckBox(
                         modifier = Modifier
@@ -114,55 +110,86 @@ fun ReportScreen(
                         }
                     )
                 }
-            }
 
-            // 마지막 항목이 선택된 경우에만 TitleText 표시
-            if (selectedId == lastItemId) {
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Box(
-                    modifier = Modifier.padding(horizontal = 20.dp)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(48.dp)
-                            .clip(RoundedCornerShape(12.dp))
-                            .background(BTN_BACKGROUND)
-                            .padding(horizontal = 20.dp),
-                        contentAlignment = Alignment.CenterStart
-                    ) {
-                        MKungTextField(
-                            textStyle = MaterialTheme.typography.bodySmall.copy(color = Color.White),
-                            placeholder = stringResource(R.string.report_reason_input),
-                            placeholderColor = GREY03,
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                            text = reportComment,
-                            onChangedText = { newValue ->
-                                reportComment = newValue
-                            },
+                if (selectedId == lastItemId) {
+                    item {
+                        Spacer(modifier = Modifier.height(8.dp))
+                        ReportCommentInput(
+                            reportComment = reportComment,
+                            onCommentChange = { reportComment = it }
                         )
                     }
                 }
             }
-
-            Spacer(modifier = Modifier.weight(1F))
-
-            MKungBtn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp)
-                    .padding(horizontal = 20.dp),
-                enable = selectedId != 0 &&
-                        (selectedId != lastItemId || reportComment.isNotBlank()),
-                // 선택된 ID가 0이 아니며, 기타 항목이 선택된 경우나 reportComment 가 비어있지 않으면 활성화
-                text = stringResource(R.string.submit)
-            ) {
-
-            }
-
-            Spacer(modifier = Modifier.height(28.dp))
         }
+
+        SubmitButton(
+            isEnabled = enableSubmit,
+            isKeyboardVisible = isKeyboardVisible,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(
+                    start = if (isKeyboardVisible) 0.dp else 20.dp,
+                    end = if (isKeyboardVisible) 0.dp else 20.dp,
+                    bottom = if (isKeyboardVisible) 0.dp else 28.dp
+                )
+        )
+    }
+}
+
+@Composable
+fun ReportCommentInput(
+    reportComment: String,
+    onCommentChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.height(120.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(BTN_BACKGROUND)
+                .padding(horizontal = 20.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            MKungTextField(
+                textStyle = MaterialTheme.typography.bodySmall.copy(color = Color.White),
+                placeholder = stringResource(R.string.report_reason_input),
+                placeholderColor = GREY03,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                text = reportComment,
+                onChangedText = onCommentChange
+            )
+        }
+    }
+}
+
+@Composable
+fun SubmitButton(
+    isEnabled: Boolean,
+    isKeyboardVisible: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    TextButton(
+        modifier = modifier
+            .height(48.dp)
+            .background(
+                color = if (isEnabled) POINT01 else GREY05,
+                shape = if (isKeyboardVisible) RoundedCornerShape(0.dp) else RoundedCornerShape(16.dp)
+            ),
+        enabled = isEnabled,
+        onClick = onClick
+    ) {
+        Text(
+            text = stringResource(R.string.submit),
+            style = MaterialTheme.typography.titleSmall,
+            color = if (isEnabled) BLACK else DOT
+        )
     }
 }
 
